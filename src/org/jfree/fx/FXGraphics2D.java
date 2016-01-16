ff266b00345b709e201a9e2947c8b26fb7eefbf9
David Gilbert <dave@jfree.org>
2016-01-16 07:43:29 +0100

    Improved tests for paint equality.

--- src/org/jfree/fx/FXGraphics2D.java
+++ src/org/jfree/fx/FXGraphics2D.java
@@ -1931,7 +1931,8 @@ public class FXGraphics2D extends Graphics2D {
      * Returns {@code true} if the two {@code Paint} objects are equal 
      * OR both {@code null}.  This method handles
      * {@code GradientPaint}, {@code LinearGradientPaint} 
-     * and {@code RadialGradientPaint} as special cases.
+     * and {@code RadialGradientPaint} as special cases, since those classes do
+     * not override the {@code equals()} method.
      *
      * @param p1  paint 1 ({@code null} permitted).
      * @param p2  paint 2 ({@code null} permitted).
@@ -1942,6 +1943,7 @@ public class FXGraphics2D extends Graphics2D {
         if (p1 == p2) {
             return true;
         }
+            
         // handle cases where either or both arguments are null
         if (p1 == null) {
             return (p2 == null);   
@@ -1968,7 +1970,9 @@ public class FXGraphics2D extends Graphics2D {
                     && lgp1.getEndPoint().equals(lgp2.getEndPoint()) 
                     && Arrays.equals(lgp1.getFractions(), lgp2.getFractions())
                     && Arrays.equals(lgp1.getColors(), lgp2.getColors())
-                    && lgp1.getCycleMethod() == lgp2.getCycleMethod();
+                    && lgp1.getCycleMethod() == lgp2.getCycleMethod()
+                    && lgp1.getColorSpace() == lgp2.getColorSpace()
+                    && lgp1.getTransform().equals(lgp2.getTransform());
         } else if (p1 instanceof RadialGradientPaint 
                 && p2 instanceof RadialGradientPaint) {
             RadialGradientPaint rgp1 = (RadialGradientPaint) p1;
@@ -1978,7 +1982,9 @@ public class FXGraphics2D extends Graphics2D {
                     && rgp1.getFocusPoint().equals(rgp2.getFocusPoint())
                     && Arrays.equals(rgp1.getFractions(), rgp2.getFractions())
                     && Arrays.equals(rgp1.getColors(), rgp2.getColors())
-                    && rgp1.getCycleMethod() == rgp2.getCycleMethod();
+                    && rgp1.getCycleMethod() == rgp2.getCycleMethod()
+                    && rgp1.getColorSpace() == rgp2.getColorSpace()
+                    && rgp1.getTransform().equals(rgp2.getTransform());
         } else {
             return p1.equals(p2);
         }
